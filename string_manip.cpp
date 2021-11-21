a60de4e2148e1166fdf66e2ac46dede9b9c5b413
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-11-20 21:52:53 -0500

    Implement slice

--- string_manip.cpp
+++ string_manip.cpp
@@ -8,3 +8,20 @@ std::size_t count(const std::string& s, char c) {
     return result;
 }
 
+std::string slice(const std::string& s, const std::size_t start = 0, const std::size_t stop = -1, const std::size_t step = 1) {
+
+    std::string result = "";
+
+    std::size_t real_start = start % s.size();
+    std::size_t real_stop = stop % (s.size() + 1);
+
+    if (real_stop > real_start) {
+        for (std::size_t i = real_start; real_start <= i && i < real_stop; i += step) {
+            result += s[i];
+        }
+    } else {
+        for (std::size_t i = real_start; real_start >= i && i > real_stop; i += step) {
+            result += s[i];
+        }
+    }
+}
