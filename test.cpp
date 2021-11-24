d43b1470d4f2c56c8a74304e3ac0ec0d0383a011
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-11-23 23:13:34 -0500

    Add more thorough slice tests

--- test.cpp
+++ test.cpp
@@ -8,5 +8,14 @@ int main() {
     cout << manip::count("Hello", 'h') << "\n";
     cout << manip::count("Hello", 'o') << "\n";
 
-    cout << manip::slice("Hello", 1, std::nullopt, 2) << "\n";
+    cout << manip::slice("Hello", 1, nullopt, 2) << "\n";
+    cout << manip::slice("Hello", 1, nullopt, nullopt) << "\n";
+    cout << manip::slice("Hello", nullopt, nullopt, nullopt) << "\n";
+    cout << manip::slice("Hello", nullopt, nullopt, -1) << "\n";
+    cout << manip::slice("Hello", -3, nullopt, nullopt) << "\n";
+    cout << manip::slice("Hello", nullopt, -1, nullopt) << "\n";
+    cout << manip::slice("Hello", -3, 0, -1) << "\n";
+    cout << manip::slice("Hello", -3, nullopt, -1) << "\n";
+    cout << manip::slice("Hello", -4, -3, -1) << "\n";
+    cout << manip::slice("Hello", nullopt, -4, -3) << "\n";
 }
