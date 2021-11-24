e2da18a0ab23a57164209a20561f6221e1dbbe50
iwei20 <44007607+iwei20@users.noreply.github.com>
2021-11-24 10:14:17 -0500

    Test hash_str

--- test.cpp
+++ test.cpp
@@ -18,4 +18,17 @@ int main() {
     cout << manip::slice("Hello", -3, nullopt, -1) << "\n";
     cout << manip::slice("Hello", -4, -3, -1) << "\n";
     cout << manip::slice("Hello", nullopt, -4, -3) << "\n";
+
+    cout << manip::hash_str("OKOKOKOK") << "\n";
+    long long result_single = (((256LL * 'O' + 'K') * 256LL * 256LL * 256LL * 256LL * 256LL * 256LL + (256LL * 'O' + 'K') * 256LL * 256LL * 256LL * 256LL + (256LL * 'O' + 'K') * 256LL * 256LL + (256LL * 'O' + 'K')) % (long long) (1e9 + 9)); 
+    cout << result_single << "\n"; 
+    
+    long long result = 0;
+    string s = "OKOKOKOK";
+    for (char c : s) {
+        result *= 256;
+        result %= (long long)1e9 + 9;
+        result += (long long)c;
+    }
+    cout << result << "\n";
 }
