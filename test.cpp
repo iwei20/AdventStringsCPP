6504afa4b362dfafa5eb2f51221b3b733c05cb1a
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-11-23 20:10:01 -0500

    Test count

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include "string_manip.hpp"
+
+using namespace std;
+
+int main() {
+    cout << manip::count("Hello", 'c') << "\n";
+    cout << manip::count("Hello", 'h') << "\n";
+    cout << manip::count("Hello", 'o') << "\n";
+}
